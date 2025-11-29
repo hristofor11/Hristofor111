ded74281598312a7aa242d99f0edd6f211d92481
hristofor <hristoforsitnikov2@gmail.com>
2025-11-29 09:46:22 +0300

    Update and rename file.txt to file.php

---
+++ file.php
@@ -0,0 +1,15 @@
+Magento Version: 2.4.6-p8 Open Source
+
+Third-party modules: LOF Multivendor (Landofcoder)
+
+Server Specs (InterServer Dedicated):
+
+CPU: AMD Ryzen 9 5950X (16-Core)
+
+RAM: 128GB DDR4
+
+Disk: HGST Enterprise SATA 12TB (no RAID)
+
+OS: Ubuntu 20.04 LTS
+
+No control panel (CLI only)
